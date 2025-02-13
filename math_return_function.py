ecf3649865353482f85cba048de2c6a4f9962160
Arganata <arganata.on@gmail.com>
2025-02-13 11:24:15 +0700

    Create math_return_function.py

---
+++ math_return_function.py
@@ -0,0 +1,26 @@
+# return = statement used to end a function
+#          and send a result back to the caller
+def add(x, y):
+    z = x + y
+    return z
+
+
+def subtract(x, y):
+    z = x - y
+    return z
+
+
+def multiply(x, y):
+    z = x * y
+    return z
+
+
+def divide(x, y):
+    z = x / y
+    return z
+
+
+print(f"Penjumlahan: {add(1, 2)}")
+print(f"Pengurangan: {subtract(1, 2)}")
+print(f"Perkalian: {multiply(1, 2)}")
+print(f"Pembagian: {divide(1, 2)}")
